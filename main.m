21ee64ea7ff5f4bca63a09d195284af9b94dfd94
Soichi Hayashi <soichih@gmail.com>
2017-02-04 23:23:53 +0000

    fixed finished output writing

--- main.m
+++ main.m
@@ -1,18 +1,21 @@
 %Simulated Annealing optimization of a given function
 
 function [] = main()
-    filenumber = num2str(1000);
-    outfilename = sprintf ( '%s%s%s', 'rosen-sa-opt', filenumber, '.dat' );
-    fileID = fopen(outfilename,'w');
 
-    ObjectiveFunction = @simple_objective;
-    rng('shuffle');
-    for n = 1:1:5
-        rx = rand(1:2)*100.0;
-        X0 = [2.0 2.0] + rx;   % Starting point
-        [x,fval,exitFlag,output] = simulannealbnd(ObjectiveFunction,X0) %
-        fprintf(fileID,'f= %12.6f  x= %9.5f   y= %9.5f x0= %9.5f  y0=%9.5f\n', fval, x, X0 );
-    end
-    fclose(fileID);
+filenumber = num2str(1000);
+outfilename = sprintf ( '%s%s%s', 'rosen-sa-opt', filenumber, '.dat' );
+fileID = fopen(outfilename,'w');
 
-    save('finished', 0);
+ObjectiveFunction = @simple_objective;
+rng('shuffle');
+for n = 1:1:5
+    rx = rand(1:2)*100.0;
+    X0 = [2.0 2.0] + rx;   % Starting point
+    [x,fval,exitFlag,output] = simulannealbnd(ObjectiveFunction,X0) %
+    fprintf(fileID,'f= %12.6f  x= %9.5f   y= %9.5f x0= %9.5f  y0=%9.5f\n', fval, x, X0 );
+end
+fclose(fileID);
+
+% output finished file
+fileID = fopen('finished','w');
+fprintf(fileID,'0');
